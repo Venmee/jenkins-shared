22e00c2393a672a33f8c8cc915d7b40b8f254753
Rajesh <rajesh.reddy@urjanet.com>
2019-09-09 13:03:17 +0530

    initial commit

---
+++ vars/sendSlack.groovy
@@ -0,0 +1,26 @@
+#!groovy
+@NonCPS
+def getChangeString() {
+  MAX_MSG_LEN = 100
+  def changeString = ""
+
+  echo "Gathering SCM changes"
+  def changeLogSets = currentBuild.changeSets
+  for (int i = 0; i < changeLogSets.size(); i++) {
+    def entries = changeLogSets[i].items
+    for (int j = 0; j < entries.length; j++) {
+      def entry = entries[j]
+      truncated_msg = entry.msg.take(MAX_MSG_LEN)
+      changeString += " - ${truncated_msg} [${entry.author}]\n"
+    }
+  }
+
+  if (!changeString) {
+    changeString = " - No new changes"
+  }
+  return changeString
+}
+
+def sendSlack(status) {
+  slackSend (color: '#80D2DE', message: "Changes:\n " + getChangeString() + "\n\n")
+}
