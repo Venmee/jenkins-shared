a624306245f2d6337f82f126bc294641405826f0
Rajesh <rajesh.reddy@urjanet.com>
2019-09-09 13:15:24 +0530

    initial commit

--- vars/sendSlack.groovy
+++ vars/sendSlack.groovy
@@ -1,26 +1,59 @@
-#!groovy
-@NonCPS
-def getChangeString() {
-  MAX_MSG_LEN = 100
-  def changeString = ""
+def call(String buildStatus) {
+  def status = buildStatus ?: 'SUCCESS'
+  def color = '#e3e4e6'
+  def statusMessage = status
 
-  echo "Gathering SCM changes"
-  def changeLogSets = currentBuild.changeSets
-  for (int i = 0; i < changeLogSets.size(); i++) {
-    def entries = changeLogSets[i].items
-    for (int j = 0; j < entries.length; j++) {
-      def entry = entries[j]
-      truncated_msg = entry.msg.take(MAX_MSG_LEN)
-      changeString += " - ${truncated_msg} [${entry.author}]\n"
+  if (status == 'STARTED') {
+    color = '#e3e4e6'
+    statusMessage = 'Started'
+  }
+  if (status == 'SUCCESS') {
+    color = 'good'
+    statusMessage = 'Success'
+  }
+  if (status == 'FAILURE') {
+    color = 'danger'
+    statusMessage = 'FAILURE'
+  }
+  if (status == 'ABORTED') {
+    color = 'warning'
+    statusMessage = 'Aborted'
+  }
+  if (status == 'NOT_BUILT') {
+    color = 'warning'
+    statusMessage = 'Not built'
+  }
+  if (status == 'UNSTABLE') {
+    color = 'danger'
+    statusMessage = 'Unstable'
+  }
+
+  
+  def getChangeString() {
+    MAX_MSG_LEN = 100
+    def changeString = ""
+
+    echo "Gathering SCM changes"
+    def changeLogSets = currentBuild.changeSets
+    for (int i = 0; i < changeLogSets.size(); i++) {
+      def entries = changeLogSets[i].items
+      for (int j = 0; j < entries.length; j++) {
+        def entry = entries[j]
+        truncated_msg = entry.msg.take(MAX_MSG_LEN)
+        changeString += " - ${truncated_msg} [${entry.author}]\n"
+      }
     }
+
+    if (!changeString) {
+      changeString = " - No new changes"
+    }
+    return changeString
   }
 
-  if (!changeString) {
-    changeString = " - No new changes"
-  }
-  return changeString
+  def message = "${env.JOB_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}> ${statusMessage}"
+    // slackSend (color: '#80D2DE', message: "Changes:\n " + getChangeString() + "\n\n")
+    slackSend (color: color, message: message + "Changes:\n " + getChangeString() )
 }
 
-def sendSlack(status) {
-  slackSend (color: '#80D2DE', message: "Changes:\n " + getChangeString() + "\n\n")
-}
+
+
