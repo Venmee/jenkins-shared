b13d221f583e74e6a46ad27a568d3323f6394218
Rajesh <rajesh.reddy@urjanet.com>
2019-09-09 18:27:56 +0530

    yet some more changes

--- vars/sendSlack.groovy
+++ vars/sendSlack.groovy
@@ -1,29 +1,30 @@
 #!/usr/bin/env groovy
 
+def getChangeString() {
+  MAX_MSG_LEN = 100
+  def changeString = ""
+
+  echo "Gathering SCM changes"
+  def changeLogSets = currentBuild.changeSets
+  for (int i = 0; i < changeLogSets.size(); i++) {
+    def entries = changeLogSets[i].items
+    for (int j = 0; j < entries.length; j++) {
+      def entry = entries[j]
+      truncated_msg = entry.msg.take(MAX_MSG_LEN)
+      changeString += " - ${truncated_msg} [${entry.author}]\n"
+    }
+  }
+
+  if (!changeString) {
+    changeString = " - No new changes"
+  }
+  return changeString
+}
+
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-
-      MAX_MSG_LEN = 100
-      def changeString = ""
-
-      echo "Gathering SCM changes"
-      def changeLogSets = currentBuild.changeSets
-      for (int i = 0; i < changeLogSets.size(); i++) {
-        def entries = changeLogSets[i].items
-        for (int j = 0; j < entries.length; j++) {
-          def entry = entries[j]
-          truncated_msg = entry.msg.take(MAX_MSG_LEN)
-          changeString += " - ${truncated_msg} [${entry.author}]\n"
-        }
-      }
-
-      if (!changeString) {
-        changeString = " - No new changes"
-      }
-      return changeString
-
-      slackSend color: "good", message: "Changes:\n " + getChangeString() + "\n By: ${entry.author}"
-    }
+    slackSend (color: '#80D2DE', message: "Changes:\n " + getChangeString() + "\n\n")
+  }
   else if( buildResult == "FAILURE" ) {
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
   }
