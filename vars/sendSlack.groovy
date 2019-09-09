ff634e11cdf2ef83bbecdd5eb9f7ccf14afee97e
Rajesh <rajesh.reddy@urjanet.com>
2019-09-09 18:23:38 +0530

    yet some more changes

--- vars/sendSlack.groovy
+++ vars/sendSlack.groovy
@@ -1,56 +1,36 @@
-def call(String buildStatus) {
-  def status = buildStatus ?: 'SUCCESS'
-  def color = '#e3e4e6'
-  def statusMessage = status
-  MAX_MSG_LEN = 100
-  def changeString = ""
+#!/usr/bin/env groovy
 
-  if (status == 'STARTED') {
-    color = '#e3e4e6'
-    statusMessage = 'Started'
-  }
-  if (status == 'SUCCESS') {
-    color = 'good'
-    statusMessage = 'Success'
-  }
-  if (status == 'FAILURE') {
-    color = 'danger'
-    statusMessage = 'FAILURE'
-  }
-  if (status == 'ABORTED') {
-    color = 'warning'
-    statusMessage = 'Aborted'
-  }
-  if (status == 'NOT_BUILT') {
-    color = 'warning'
-    statusMessage = 'Not built'
-  }
-  if (status == 'UNSTABLE') {
-    color = 'danger'
-    statusMessage = 'Unstable'
-  }
+def call(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
 
-  echo "Gathering SCM changes"
-  def changeLogSets = currentBuild.changeSets
-  for (int i = 0; i < changeLogSets.size(); i++) {
-    def entries = changeLogSets[i].items
-    for (int j = 0; j < entries.length; j++) {
-      def entry = entries[j]
-      truncated_msg = entry.msg.take(MAX_MSG_LEN)
-      changeString += " - ${truncated_msg} [${entry.author}]\n"
-    }
-  }
+      MAX_MSG_LEN = 100
+      def changeString = ""
 
-  if (!changeString) {
-    changeString = " - No new changes"
-  }
-  
-  return changeString
+      echo "Gathering SCM changes"
+      def changeLogSets = currentBuild.changeSets
+      for (int i = 0; i < changeLogSets.size(); i++) {
+        def entries = changeLogSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+          def entry = entries[j]
+          truncated_msg = entry.msg.take(MAX_MSG_LEN)
+          changeString += " - ${truncated_msg} [${entry.author}]\n"
+        }
+      }
 
-  def message = "${env.JOB_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}> ${statusMessage}"
-    // slackSend (color: '#80D2DE', message: "Changes:\n " + getChangeString() + "\n\n")
-    slackSend (color: color, message: message + "Changes:\n " + getChangeString() )
+      if (!changeString) {
+        changeString = " - No new changes"
+      }
+      return changeString
+
+      slackSend color: "good", message: "Changes:\n " + getChangeString() + "\n By: ${entry.author}"
+
+  else if( buildResult == "FAILURE" ) {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+  }
+  else if( buildResult == "UNSTABLE" ) {
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+  }
+  else {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"
+  }
 }
-
-
-
